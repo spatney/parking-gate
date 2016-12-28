a8a20f80546c0c49153422b8cfc837503f3baf64
Sachin Patney <spatney@microsoft.com>
2016-12-29 00:18:37 +0800

    init

---
+++ server.js
@@ -0,0 +1,18 @@
+var app = require('express')();
+var server = require('http').createServer(app);
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+app.post('/gate', function(req, res){
+    console.log('command ->', req.body.command)
+    res.json({echo: req.body.command})
+});
+
+server.listen(1337, function(){
+    console.log('listening ....')
+})
