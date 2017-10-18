bbb70cd35f91778d7cc4fa9fa206f18b9263a058
Sachin Patney <spatney@microsoft.com>
2017-10-18 23:45:18 +0800

    update

--- deviceController.js
+++ deviceController.js
@@ -1,12 +1,12 @@
-/*var Relay = require('./relay');
-var Gate = require('./piGate');*/
+var Relay = require('./relay');
+var Gate = require('./piGate');
 var RGBLed = require('./rgbLed');
 
-/*var vacuum = new Relay(16);
+var vacuum = new Relay(16);
 var water = new Relay(36);
 var jet = new Relay(37);
-var gate = new Gate(38, 40, 33); // (pulse, direction, enable)*/
-var ledStrip = new RGBLed(12, 32, 35); // (red, green, blue)
+var gate = new Gate(38, 40, 33); // (pulse, direction, enable)
+//var ledStrip = new RGBLed(12, 32, 35); // (red, green, blue)
 
 class DeviceController {
     constructor(socket) {
