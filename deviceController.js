4753ce582c0133fec00e60e0f0c1a65663409a7e
Sachin Patney <spatney@microsoft.com>
2017-10-18 22:10:21 +0800

    enable

--- deviceController.js
+++ deviceController.js
@@ -1,4 +1,4 @@
-/*var OutputPort = require('./outputPort');
+var OutputPort = require('./outputPort');
 var Relay = require('./relay');
 var Gate = require('./piGate');
 var RGBLed = require('./rgbLed');
@@ -7,7 +7,7 @@ var vacuum = new Relay(16);
 var water = new Relay(36);
 var jet = new Relay(37);
 var gate = new Gate(38, 40, 33); // (pulse, direction, enable)
-var ledStrip = new RGBLed(12, 32, 35); // (red, green, blue)*/
+var ledStrip = new RGBLed(12, 32, 35); // (red, green, blue)
 
 class DeviceController {
     constructor(socket) {
@@ -21,35 +21,35 @@ class DeviceController {
                 const state = command.state;
                 if (state) {
                     console.log('gate open');
-                    //gate.open();
+                    gate.open();
                 } else {
                     console.log('gate close');
-                    //gate.close();
+                    gate.close();
                 }
                 break;
 
             case 'water':
                 console.log('water pulse on');
-                //water.state(true);
+                water.state(true);
                 setTimeout(() => {
                     console.log('water pulse off');
-                    //water.state(false);
+                    water.state(false);
                 }, 50);
                 break;
             case 'jet':
                 console.log('jet pulse on');
-                //jet.state(true);
+                jet.state(true);
                 setTimeout(() => {
                     console.log('jet pulse off');
-                    //jet.state(false);
+                    jet.state(false);
                 }, 50);
                 break;
             case 'vacuum':
                 console.log('vacuum pulse on');
-                //vacuum.state(true);
+                vacuum.state(true);
                 setTimeout(() => {
                     console.log('vacuum pulse off');
-                    //vacuum.state(false);
+                    vacuum.state(false);
                 }, 50);
                 break;
 
@@ -60,7 +60,7 @@ class DeviceController {
 
                 console.log(`rgb -> ${red},${green},${blue}`);
 
-            //ledStrip.setColor(red, green, blue);
+            ledStrip.setColor(red, green, blue);
             break;
 
             default: console.error('command unknown', JSON.stringify(command));
